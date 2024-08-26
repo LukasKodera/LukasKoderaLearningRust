a5c69f6b55a9f09be3c4703d1d8d65db1b0c8bf0
Lukas Kodera <lukaskodera.lk@gmail.com>
2024-08-26 17:28:39 +0200

    feat: add testingProject

---
+++ helloProject/target/debug/deps/helloProject-724baeb5f78cb356.d
@@ -0,0 +1,5 @@
+/home/zack/GitHub/LukasKoderaLearningRust/helloProject/target/debug/deps/helloProject-724baeb5f78cb356: src/main.rs
+
+/home/zack/GitHub/LukasKoderaLearningRust/helloProject/target/debug/deps/helloProject-724baeb5f78cb356.d: src/main.rs
+
+src/main.rs:
---
+++ helloProject/target/debug/helloProject.d
@@ -0,0 +1,1 @@
+/home/zack/GitHub/LukasKoderaLearningRust/helloProject/target/debug/helloProject: /home/zack/GitHub/LukasKoderaLearningRust/helloProject/src/main.rs
