1aaf79239e2cdcc83a6204a0498db6ead4106884
Lukas Kodera <lukaskodera.lk@gmail.com>
2024-09-15 17:35:33 +0200

    add information

--- testingProject/testing.rs
+++ testingProject/testing.rs
@@ -1,6 +1,30 @@
 fn main(){
-    let x: i32 = 42;
-    let y: u64 = 100;
-    println!("Signed Integer: {}", x);
-    println!("Unsigned Integer: {}", y);    
+
+    //let x: i32 = 42;
+    //let y: u64 = 100;
+    //println!("Signed Integer: {}", x);
+    //println!("Unsigned Integer: {}", y); 
+
+
+    //let is_done: bool = true;
+    //println!("Is it done? {}", is_done);
+
+    //let letter: char = 'a';
+    //println!("First letter of the alphabet is: {}", letter);
+    
+    // let numbers: [i32; 5] = [1,2,3,4,5];
+    // on second position when defining array, there is
+    // size of array
+    // println!("Number Array: {:?}", numbers);
+
+    // let fruits: [&str; 3] = ["Apple", "Banana", "Peach"];
+    // println!("Our fruits are: {:?}", fruits);
+    // return specific fruits, for example first element from list
+    // println!("Our fruit is: {}", fruits[0]);
+    
+    
+
+
+
+
 }
