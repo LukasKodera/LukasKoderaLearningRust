a35fd2859fceb03b7597058da01d5766f94573ab
Lukas Kodera <lukaskodera.lk@gmail.com>
2024-10-06 18:03:26 +0200

    add information

--- testingProject/testing.rs
+++ testingProject/testing.rs
@@ -22,7 +22,27 @@ fn main(){
     // return specific fruits, for example first element from list
     // println!("Our fruit is: {}", fruits[0]);
     
+    // let human: (String, i32, bool) = ("Alice".to_string(), 30, false);
+    // println!("Human Tuple: {:?}", human);
     
+    // let my_mix_tuple = ("Kratos", 23, true, [1,2,3,4,5]);
+    // println!("Mix tuple: {:?}", my_mix_tuple);
+
+    // let number_slices:&[i32] = &[1,2,3,4,5]; 
+    // println!("Number Slice: {:?}", number_slices);
+
+    // let animal_slices :&[&str] = &["Dog", "Mouse", "Chimp", "Lukáš :-D"]; 
+    //println!("Animal slices: {:?}", animal_slices);
+
+    let mut stone_cold: String = String::from("Hell, ");
+    stone_cold.push_str("Yeah!");    
+    println!("Stone Cold Says: {}", stone_cold);
+
+
+
+
+
+
 
 
 
